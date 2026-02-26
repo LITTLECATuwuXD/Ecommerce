d1984d1ca96ae1cf80b4677af4af1af005bcc74a
Gatito_uwu <suedamazeravla@gmail.com>
2026-02-26 00:35:16 -0600

    0.1.7-Agregando el primer link en Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => 
 {
     return(
         <section className="header">
-            <h1 className="Logo">DAN<span>TALIAN</span></h1>
+            <h1 className="Logo">DAN<span>talian</span></h1>
+            <nav className="navbar">
+                <ul className="nav-links">
+                    <li>
+                        <link to="/">Home</link>
+                    </li>
+                </ul>
+            </nav>
         </section>
     )
 }
