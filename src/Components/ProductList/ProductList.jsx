7a39629f3722e82068f2ea2d283c7ff4253a682f
Gatito_uwu <suedamazeravla@gmail.com>
2026-02-26 01:55:53 -0600

    0.2.3-Agregando un elemento de la lista en ProductList.jsx

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -6,6 +6,15 @@ const ProductList = () =>
         <section className="main-content">
             <aside className="filters">
                 <h2>Filtro</h2>
+                <div className="filters-category">
+                    <div className="filter-category">
+                        <h3>Categorías</h3>
+                        <label>
+                            <input type="checkbox"/>
+                            <span>Hombres</span>
+                        </label>
+                    </div>
+                </div>
             </aside>
         </section>
     )
