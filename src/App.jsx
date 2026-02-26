1cf2a781b3ca84e1e6d080f0724a470c7518681f
Gatito_uwu <suedamazeravla@gmail.com>
2026-02-25 23:49:46 -0600

    0.1.3-Importando a Navbar dentro de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./Components/Home/Home"
+import Navbar from "./Components/Navbar/Navbar"
 function App() {
 
   return (
     <>
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
