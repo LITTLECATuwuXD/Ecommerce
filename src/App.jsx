00b0bd3269c7ec573df6b6d8f8d8c7de32438bc9
Gatito_uwu <suedamazeravla@gmail.com>
2026-03-29 06:11:01 -0600

    0.7.5-Importando CartContex.jsx a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./Components/Home/Home"
 import Navbar from "./Components/Navbar/Navbar"
+import {CartProvider} from "./Components/CartContext/CartContext"
 function App() {
 
   return (
     <>
+    <CartProvider>
     <Router>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
     </Router>
+    </CartProvider>
     </>
   )
 }
