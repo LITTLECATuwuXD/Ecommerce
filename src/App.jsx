7ae2e90f540bc19e64f7aa9ece6655ae5ce4b231
Gatito_uwu <suedamazeravla@gmail.com>
2026-03-29 07:31:40 -0600

    0.7.7-Agregando DetailsProduct.jsx a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./Components/Home/Home"
 import Navbar from "./Components/Navbar/Navbar"
 import {CartProvider} from "./Components/CartContext/CartContext"
+import DetailsProduct from "./Components/DetailsProduct/DetailsProduct"
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
+        <Route path="/producto/:id" element={<DetailsProduct/>}></Route>
       </Routes>
     </Router>
     </CartProvider>
