9ea4358cbd1280e9380e8a95190ae735bbe5ce0e
Gatito_uwu <suedamazeravla@gmail.com>
2026-03-29 06:00:57 -0600

    0.7.3-Agregamos un const para agregar/actualizar el carrito en CartContext.jsx

--- src/Components/CartContext/CartContext.jsx
+++ src/Components/CartContext/CartContext.jsx
@@ -4,6 +4,22 @@ const CartContext = createContext();
 
 const CartProvider = () =>{
     const [carrito, setCarrito]=useState([]);
+
+    const agregarAlcarrito =(producto)=>{
+        setCarrito((carritoAnterior)=>{
+            const yaExisteElproducto=carritoAnterior.findIndex(
+                (articulo)=>articulo.id===producto.id
+            );
+            if(yaExisteElproducto>=0){
+                const carritoActualizado = [...carritoAnterior];
+                carritoActualizado[yaExisteElproducto].cantidad+1;
+                return carritoActualizado;
+            }
+            else{
+                return [...carritoAnterior,{...producto,cantidad:1}]
+            }
+        })
+    }
     return (
         <div>CartContext</div>
     )
