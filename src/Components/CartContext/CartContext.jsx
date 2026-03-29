d1342f69b64a01376a93d886a6988043b34d9763
Gatito_uwu <suedamazeravla@gmail.com>
2026-03-29 05:49:00 -0600

    0.7.2-Agregamos un const para guardar las cosas del carrito en CartContext.jsx

--- src/Components/CartContext/CartContext.jsx
+++ src/Components/CartContext/CartContext.jsx
@@ -3,6 +3,7 @@ import React, {createContext} from "react";
 const CartContext = createContext();
 
 const CartProvider = () =>{
+    const [carrito, setCarrito]=useState([]);
     return (
         <div>CartContext</div>
     )
