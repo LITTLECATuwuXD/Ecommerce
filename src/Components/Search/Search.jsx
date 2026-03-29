e2723b9440b1500f72dc5b57278b057c3e390825
Gatito_uwu <suedamazeravla@gmail.com>
2026-03-29 15:32:28 -0600

    0.11.0-Creando un buscador

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Search.css"
+
+const Search = () =>{
+    return (
+        <section className="search">
+            <input type="search"
+            placeholder="buscar"
+            className="search-bar"
+            />
+        </section>
+    )
+}
+
+export default Search
